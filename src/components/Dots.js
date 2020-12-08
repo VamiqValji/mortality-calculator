77bbb2704d04652a87918d3cabe07d00ff95b5c7
Vamiq <vamiq.valji@gmail.com>
2020-12-07 20:43:44 -0500

    Dots are changing without refreshing the page!
    React is only re-rendering changing components, and I got the dots to render based on a value passed down to its component through App.js. This will change to a number corresponding to the birth date of the user, and will make more sense later down the road.

--- src/components/Dots.js
+++ src/components/Dots.js
@@ -7,10 +7,14 @@ function Dots({dots}) {
     // ReactDOM.render(li, document.getElementsByClassName("dots"));
     
     let numbers = [];
-    // let i = dots;
-    for (i = 0; i <= dots; i--) {
-        numbers.push(1);
-      }
+    
+    let i = 0;
+
+    while (i < dots + 1) {
+        i = i + 1;
+        numbers.push(i);
+        console.log(numbers);
+    }
 
     const listItems = numbers.map((number) => // for each number in numbers[]
     <span>•</span>);
