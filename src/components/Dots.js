c512133c294fb15e1b3ea027789b2f2f1ede176c
Vamiq <vamiq.valji@gmail.com>
2020-12-07 19:24:21 -0500

    Basic Concept of DOM Rendering
    The basic understanding of how DOM rendering will be implemented in this project is set.

--- src/components/Dots.js
+++ src/components/Dots.js
@@ -1,9 +1,23 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 function Dots() {
+
+    // let li = React.createElement("li", {className="li"}, "test");
+    // ReactDOM.render(li, document.getElementsByClassName("dots"));
+
+    const numbers = [1,1,1,1,1,1];
+    const listItems = numbers.map((number) =>
+    <li>•</li>);
+
+    ReactDOM.render(
+        <ul>{listItems}</ul>,
+        document.getElementById("dots")
+      );
+
     return(
         <div>
-            <div>Dots</div>
+            
             <li></li>
             <li></li>
             <li></li>
