097750219955ff8cc6d7ccce3ecc50cf273c895a
Vamiq <vamiq.valji@gmail.com>
2020-12-08 21:22:13 -0500

    Component Rendering Issue
    I am currently trying to render out the dots depending on a condition, then assigning a class based on that condition.

--- src/components/Dots.js
+++ src/components/Dots.js
@@ -7,15 +7,10 @@ function Dots({birthdate}) {
     // ReactDOM.render(li, document.getElementsByClassName("dots"));
     
     let numbers = [];
+    let livedDots = [];
+    let unlivedDots = [];
     
     let i = 0;
-
-    const lifeExpectancyMonths = 870; /* 72.563 * 12 = 870.756 */ /* factors of 870: 1, 2, 3, 5, 6, 10, 15, 29, 30, 58, 87, 145, 174, 290, 435, 870 */
-
-    while (i < lifeExpectancyMonths + 1) {
-        i = i + 1;
-        numbers.push(i);
-    }
 
     let birthMonth = parseInt(birthdate.slice(5,7));
     let birthYear = parseInt(birthdate.slice(0,4));
@@ -25,6 +20,8 @@ function Dots({birthdate}) {
     let monthsLived = 0;
 
     let output = "";
+
+    const lifeExpectancyMonths = 870; /* 72.563 * 12 = 870.756 */ /* factors of 870: 1, 2, 3, 5, 6, 10, 15, 29, 30, 58, 87, 145, 174, 290, 435, 870 */
 
     if (isNaN(birthMonth) === true || isNaN(birthYear) === true) {
         console.log("Invalid input.");
@@ -36,9 +33,41 @@ function Dots({birthdate}) {
         output = "You have lived " + monthsLived + " months."
     }
 
+    while (i <= lifeExpectancyMonths) {
+        i = i + 1;
+        numbers.push(i);
+        if (i < 5) {
+            livedDots.push(i)
+        }
+        else {
+            unlivedDots.push(i)
+        }
+    }
+
+    let livedDotsMap = []
+    let unlivedDotsMap = []
+
+    let livedDotsMap = livedDots.map((number) => // for each number in numbers[]
+    livedDotsMap.push(<span className="liveDot">•</span>)
+    );
+
+    let unlivedDotsMap = unlivedDots.map((number) => // for each number in numbers[]
+    unlivedDotsMap.push(<span className="unliveDot">•</span>)
+    );
+    console.log(livedDotsMap,unlivedDotsMap);
+    console.log(livedDotsMap + unlivedDotsMap);
+
+    // const listItems = livedDotsMap + unlivedDotsMap;
+
     const listItems = numbers.map((number) => // for each number in numbers[]
     <span>•</span>
     );
+
+    const listItems = numbers.map((number) => // for each number in numbers[]
+    <span>•</span>
+    );
+
+    console.log(listItems);
 
     ReactDOM.render(
         <li className="dotsContainer">{listItems}</li>,
