032f4caf2c4ef33ac8d88191897a34538151c6fd
Vamiq <vamiq.valji@gmail.com>
2020-12-08 22:05:13 -0500

    Months Lived Displayed!
    Your months lived are now displayed as a different color than months still left to be lived.

--- src/components/Dots.js
+++ src/components/Dots.js
@@ -7,8 +7,6 @@ function Dots({birthdate}) {
     // ReactDOM.render(li, document.getElementsByClassName("dots"));
     
     let numbers = [];
-    let livedDots = [];
-    let unlivedDots = [];
     
     let i = 0;
 
@@ -19,55 +17,32 @@ function Dots({birthdate}) {
     let yearsLived = 0;
     let monthsLived = 0;
 
-    let output = "";
+    let outputLivedMonths = "";
 
     const lifeExpectancyMonths = 870; /* 72.563 * 12 = 870.756 */ /* factors of 870: 1, 2, 3, 5, 6, 10, 15, 29, 30, 58, 87, 145, 174, 290, 435, 870 */
 
     if (isNaN(birthMonth) === true || isNaN(birthYear) === true) {
         console.log("Invalid input.");
-        output = "Please enter a valid input."
+        outputLivedMonths = "Please enter a valid input."
     } else {
         yearsLived = (currentYear - birthYear) 
         monthsLived = (yearsLived * 12) + (currentMonth - birthMonth);
-        console.log(monthsLived + " months lived.");
-        output = "You have lived " + monthsLived + " months."
+        outputLivedMonths = "You have lived approximately " + monthsLived + " months of the average life expectancy, or " + ((monthsLived / lifeExpectancyMonths) * 100).toFixed(2) + "% of your life."
     }
 
     while (i <= lifeExpectancyMonths) {
         i = i + 1;
         numbers.push(i);
-        if (i < 5) {
-            livedDots.push(i)
-        }
-        else {
-            unlivedDots.push(i)
-        }
     }
 
-    let livedDotsMap = []
-    let unlivedDotsMap = []
-
-    let livedDotsMap = livedDots.map((number) => // for each number in numbers[]
-    livedDotsMap.push(<span className="liveDot">•</span>)
-    );
-
-    let unlivedDotsMap = unlivedDots.map((number) => // for each number in numbers[]
-    unlivedDotsMap.push(<span className="unliveDot">•</span>)
-    );
-    console.log(livedDotsMap,unlivedDotsMap);
-    console.log(livedDotsMap + unlivedDotsMap);
-
-    // const listItems = livedDotsMap + unlivedDotsMap;
-
-    const listItems = numbers.map((number) => // for each number in numbers[]
-    <span>•</span>
-    );
-
-    const listItems = numbers.map((number) => // for each number in numbers[]
-    <span>•</span>
-    );
-
-    console.log(listItems);
+    const listItems = numbers.map((number) => { // for each number in numbers[]
+    if (number < monthsLived) {
+        return <span className="livedDots">•</span>
+    }
+    else {
+        return <span className="unlivedDots">•</span>
+    }
+    });
 
     ReactDOM.render(
         <li className="dotsContainer">{listItems}</li>,
@@ -76,7 +51,8 @@ function Dots({birthdate}) {
 
     return(
         <div>
-            <div>{output}</div>
+            <div>{outputLivedMonths}</div>
+            <div className="wisely">Use your time wisely.</div>
         </div>
     )
 }
