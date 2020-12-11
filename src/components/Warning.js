b5caffff1b697ece74dab7c5ede4192b1a3419a1
Vamiq <vamiq.valji@gmail.com>
2020-12-10 21:46:51 -0500

    Working on warning screen // styling
    Working on the warning screen and styling!

--- src/components/Warning.js
+++ src/components/Warning.js
@@ -16,9 +16,12 @@ function Warning() {
     return (
     <div>
         <div className={toggle ? "" : "warningActive"}>
-            <h1>Hello React</h1>
-            {/* {toggle ? 'active' : ""} MEANS toggle (variable state) class on and off if true, otherwise class is "" (nothing) */}
-            <button onClick={toggleActive}>Click</button>
+            <div className="warningDiv">
+                <h1 className="warningTitle">Notice to User</h1>
+                <div classname="warningText">Take all of the following information with a grain of salt, as such sensitive topics and dates can't be defined by a defined by this simple calculator. If you or someone you know is feeling suicidal, please reach out or help them reach out by calling <a href="https://suicidepreventionlifeline.org/talk-to-someone-now/">1-800-273-8255 (United States)</a>. More information <a href="https://suicidepreventionlifeline.org/">here (United States)</a>.</div>
+                {/* {toggle ? 'active' : ""} MEANS toggle (variable state) class on and off if true, otherwise class is "" (nothing) */}
+                <button className="warningButton" onClick={toggleActive}>Click</button>
+            </div>
         </div>
     </div>
     );
