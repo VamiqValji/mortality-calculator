091ec52b6c51863a3856565422c6307c6ea28179
Vamiq <vamiq.valji@gmail.com>
2020-12-14 19:22:30 -0500

    node commands dont seem to register.
    weird.

--- src/components/PastSubmissions.js
+++ src/components/PastSubmissions.js
@@ -3,27 +3,35 @@ import ReactDOM from "react-dom";
 
 function PastSubmissions({bd, li}) {
 
-    const [birthdays, setBirthdays] = useState([]);
-    const [lives, setLives] = useState([]);
-    if (li !== "" || bd !== "") {
-        setBirthdays(prev => prev.push(bd));
-        setLives(prev => prev.push(li));
-    }
+//     const [birthdays, setBirthdays] = useState([]);
+//     const [lives, setLives] = useState([]);
+//     if (li !== "" || bd !== "") {
+//         setBirthdays(prev => prev.push(bd));
+//         setLives(prev => prev.push(li));
+//     }
     
-    let bdSubmissions = birthdays.map(() => { // for each number in numbers[]
-        return <li>Birthdate: {bd}</li>
-    });
+//     let bdSubmissions = birthdays.map(() => { // for each number in numbers[]
+//         return <li>Birthdate: {bd}</li>
+//     });
 
-    let liSubmissions = lives.map(() => { // for each number in numbers[]
-        return <li>Life Expectancy: {li}</li>
-    });
+//     let liSubmissions = lives.map(() => { // for each number in numbers[]
+//         return <li>Life Expectancy: {li}</li>
+//     });
 
-    ReactDOM.render(<div><ul>{bdSubmissions}</ul> <ul>{liSubmissions}</ul></div>, document.getElementById("submissions"));
-  return (
-    <div>
-      <div className="psTitle">Past Submissions</div>
-    </div>
-  );
+//     ReactDOM.render(<div><ul>{bdSubmissions}</ul> <ul>{liSubmissions}</ul></div>, document.getElementById("submissions"));
+
+    const fs = require("fs");
+    // if (fs.readFile) {
+
+    // }
+    fs.writeFile("-" + bd, "utf8")
+    fs.writeFile("-" + li, "utf8")
+//   return (
+//     <div>
+//       <div className="psTitle">Past Submissions</div>
+//     </div>
+//   );
+
 }
 
 export default PastSubmissions;
