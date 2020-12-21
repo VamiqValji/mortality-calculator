362b4c8e49b68f0d24d0e3bafc71c3e8ec99f277
Vamiq <vamiq.valji@gmail.com>
2020-12-21 16:28:24 -0500

    Database get / post
    Currently am able to add, and receive data from database

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const { mongodbURL } = require("./mongodbURL");
-const app = express;
+const app = express();
+const Blog = require("./models/bdays");
+const bday = require("./models/bdays");
 
 // connect to database
 mongoose
@@ -11,3 +13,27 @@ mongoose
   })
   .then((result) => app.listen(3001))
   .catch((err) => console.log(err));
+
+// app.get("/new", (req, res) => {
+//   const BDAY = new bday({
+//     bday: "test1",
+//   });
+//   BDAY.save().then((result) => {
+//     // saves to database
+//     res.send(result);
+//     console.log(result).catch((err) => {
+//       console.log(err);
+//     });
+//   });
+// });
+
+app.get("/", (req, res) => {
+  bday
+    .find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
